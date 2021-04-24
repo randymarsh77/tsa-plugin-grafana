fccf0b127ccd25b899e49c97243f0d7154e9d9be
Matt Preucil <matt.preucil@faithlife.com>
2021-04-24 10:53:45 -0700

    feat(initial): plugin v1.

--- README.md
+++ README.md
@@ -4,21 +4,29 @@ Grafana plugin for [tsa](https://github.com/randymarsh77/tsa-cli).
 
 ## Usage
 
+With no configuration
 ```
-// With no configuration
 tsa --plugin tsa-plugin-grafana --host cool.site.com --nodes web-node-number* --since 1d --stat cpu --metric max
+```
 
-// Set some defaults to make repeat calls easier
-tsa config --profile --default plugin=grafana
-tsa config --profile grafana --alias production=cool.site.com
-tsa config --profile grafana --default host=production
+Set some defaults to make repeat calls easier
+```
+tsa config --default plugin=tsa-plugin-grafana
+tsa config --alias production=cool.site.com
+tsa config --default host=production
+```
 
-// Ah, that is easier
+Ah, that is easier
+```
 tsa --nodes web-node-number* --since 1d --stat cpu --metric max
+```
 
-// Do you do a thing a lot?
-tsa config --profile grafana --alias max-cpu='--nodes web-node-number* --stat cpu --metric max'
+Do you do a thing a lot?
+```
+tsa config --alias max-cpu='--nodes web-node-number* --stat cpu --metric max'
+```
 
-// Even easier
+Even easier
+```
 tsa max-cpu --since 1d
 ```
--- src/index.ts
+++ src/index.ts
@@ -33,16 +33,37 @@ interface IGrafanaOptions {
 }
 
 function buildQuery(instance: string, domain: string) {
-	const query = `cpucap_cur_usage_percentage{instance=~"${instance}${domain}",job=~".*@cmon.*"} / cpucap_limit_percentage{instance=~"${instance}${domain}"} * 100`;
+	const escapedDomain = domain.replace(/\./g, '\\\\.');
+	const query = `cpucap_cur_usage_percentage{instance=~"${instance}${escapedDomain}",job=~".*@cmon.*"} / cpucap_limit_percentage{instance=~"${instance}${escapedDomain}"} * 100`;
 	return encodeURIComponent(query);
 }
 
-const execute = async (
-	{ start, end, step }: ITSAPluginArgs,
-	{ host, instance, domain, auth }: IGrafanaOptions
-) => {
+function validateGrafanaOptionsOrFail({ host, instance, domain, auth }: IGrafanaOptions) {
+	if (!host) {
+		console.error('You must provide a valid host. e.g. --host my.grafana.host.com');
+		process.exit(1);
+	}
+
+	if (!instance) {
+		console.error('You must provide a valid instance. e.g. --instance my-web-node');
+		process.exit(1);
+	}
+
+	if (!domain) {
+		console.error('You must provide a valid domain. e.g. --domain .my.company.io');
+		process.exit(1);
+	}
+}
+
+const execute = async ({ start, end, step }: ITSAPluginArgs, grafanaOptions: IGrafanaOptions) => {
+	validateGrafanaOptionsOrFail(grafanaOptions);
+
+	const { host, instance, domain, auth } = grafanaOptions;
 	const arg = buildQuery(instance, domain);
-	const range = `&start=${start}&end=${end}&step=${step}`;
+	const startSeconds = Math.round(start / 1000);
+	const endSeconds = Math.round(end / 1000);
+	const stepSeconds = Math.round(step / 1000);
+	const range = `&start=${startSeconds}&end=${endSeconds}&step=${stepSeconds}`;
 	const path = `api/datasources/proxy/1/api/v1/query_range?query=${arg}${range}`;
 	const url = `https://${host}/${path}`;
 
@@ -52,10 +73,13 @@ const execute = async (
 				Cookie: `grafana_session=${auth}`,
 			},
 		});
-		const data = response.data.data.result[0].values.map(([ts, v]: string[]) => [
-			new Date(ts),
-			parseInt(v, 10),
-		]);
+		const seriesData = response.data.data.result;
+		if (!seriesData || seriesData.length === 0) {
+			console.log('No data for specified time frame.');
+			process.exit(0);
+		}
+
+		const data = seriesData[0].values.map(([ts, v]: string[]) => [new Date(ts), parseInt(v, 10)]);
 		return data;
 	} catch (error) {
 		console.error(error);
